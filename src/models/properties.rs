7eb1b47fe3c2704782d0a44ed046dd2fcf0faa24
Ben Weber <ben@b3n.gg>
2024-02-08 12:40:10 +0100

    Make phone number property value optional

--- src/models/properties.rs
+++ src/models/properties.rs
@@ -349,7 +349,7 @@ pub enum PropertyValue {
     /// <https://developers.notion.com/reference/property-object#phone-number-configuration>
     PhoneNumber {
         id: PropertyId,
-        phone_number: String,
+        phone_number: Option<String>,
     },
     /// <https://developers.notion.com/reference/property-object#created-time-configuration>
     CreatedTime {
