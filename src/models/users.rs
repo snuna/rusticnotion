09cb3dff623a4f7930459dbb923a9bfc200a7374
Ben Weber <ben@b3n.gg>
2025-02-08 19:39:22 +0100

    Fix user property

--- src/models/users.rs
+++ src/models/users.rs
@@ -4,17 +4,21 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct UserCommon {
     pub id: UserId,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub avatar_url: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug, Eq, PartialEq)]
 pub struct Person {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub email: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug, Eq, PartialEq)]
 pub struct Bot {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub email: Option<String>,
 }
 
